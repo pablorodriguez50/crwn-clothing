89a1a7d8450f4206c46528e1ed1008f7689b5c09
PABLO RODRIGUEZ <pablorodrigueza@hotmail.cl>
2021-08-19 17:44:15 -0400

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,9 +31,8 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          })
+          });
 
-          console.log(this.state);
         });
       }
 
